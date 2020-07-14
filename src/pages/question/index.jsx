7a56444741236c3bf9cd18bf9896425c20619230
polichan <lazysloths@yahoo.com>
2020-07-14 20:04:09 +0800

    feat: 增加问题界面

---
+++ src/pages/question/index.jsx
@@ -0,0 +1,16 @@
+import Taro , { Component } from '@tarojs/taro';
+import { View, Text , Button} from '@tarojs/components';
+import StepPage from "@/components/StepPage"
+
+export default class Question extends Component {
+
+  state={}
+
+  render() {
+    return (
+        <StepPage>
+            
+        </StepPage>
+    );
+  }
+}
