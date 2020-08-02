3ed4a4b06ace0ff0c448302f498e60ad18d75092
Hexwell <lazysloths@yahoo.com>
2020-08-02 15:00:06 +0800

    feat: 抽象公共 Problems 组件，引入 Problem 组件常量

---
+++ src/components/Problem/index.jsx
@@ -0,0 +1,156 @@
+import Taro, { Component } from "@tarojs/taro";
+import { View } from "@tarojs/components";
+import { AtTabs, AtTabsPane } from "taro-ui";
+import Skeleton from "taro-skeleton";
+import Article from "@/components/Article";
+import Question from "@/components/Question";
+import { connect } from "@tarojs/redux";
+import "./index.scss";
+
+@connect(({ question, loading }) => ({
+  question,
+  loading
+}))
+export default class Problem extends Component {
+  static defaultProps = {
+    questionApi: {
+      example: () => {},
+      real: () => {}
+    },
+    data: {
+      title: "什么是特征向量和特征值?",
+      sections: [
+        "假设我们取到的是第i张图片，那么我们上一步进行了对图片分块（取patch）的过程，每一个patch对应一个矩阵，然后将矩阵变成列向量，减去对应每一列的均值，得到了一个X矩阵，然后求解X乘上X的转置。得出的矩阵的解就是我们要求的特征值和特征向量。我们可以从接下来的特征值和特征向量可视化中看出在前面的特征值和特征向量能量占比很大"
+      ]
+    },
+  };
+  state = {
+    current: 0,
+    tabList: [{ title: "题目举例" }, { title: "实战解答" }],
+    hasLoadedTabList: [0],
+    questions: {
+      example: {
+        content: ""
+      },
+      real: {
+        content: ""
+      }
+    }
+  };
+
+  componentWillMount() {
+    Taro.nextTick(() => {
+      this.getExampleQuestion().then(res => {
+        this.setState({
+          questions: Object.assign(this.state.questions, { example: res })
+        });
+      });
+    });
+  }
+
+  getExampleQuestion() {
+    return new Promise(resolve => {
+      this.props
+        .dispatch({
+          type: "question/getQuestionById",
+          payload: {
+            data: {
+              ID: this.props.questionApi.example.ID
+            }
+          }
+        })
+        .then(res => {
+          resolve(res.data.recrmProblems);
+        });
+    });
+  }
+
+  getRealQuestion() {
+    return new Promise(resolve => {
+      this.props
+        .dispatch({
+          type: "question/getQuestionById",
+          payload: {
+            data: {
+              ID: this.props.questionApi.real.ID
+            }
+          }
+        })
+        .then(res => {
+          resolve(res.data.recrmProblems);
+        });
+    });
+  }
+
+  handleTabClick(value) {
+    if (this.state.hasLoadedTabList.indexOf(value) == -1) {
+      this.setState(
+        {
+          current: value,
+          hasLoadedTabList: this.state.hasLoadedTabList.concat([value])
+        },
+        () => {
+          if (value === 1) {
+            //实战解答问题
+            this.getRealQuestion().then(res => {
+              this.setState({
+                questions: Object.assign(this.state.questions, { real: res })
+              });
+            });
+          }
+        }
+      );
+    } else {
+      this.setState({
+        current: value
+      });
+    }
+  }
+
+  render() {
+    const isQuestionLoading = this.props.loading.effects[
+        "question/getQuestionById"
+      ];
+    const { data } = this.props;
+    const { questions, tabList, current } = this.state;
+    return (
+      <View className='question-container'>
+        <View className='question-explain'>
+          <Article sections={data.sections}></Article>
+        </View>
+        <AtTabs
+          current={current}
+          tabList={tabList}
+          onClick={this.handleTabClick.bind(this)}
+        >
+          <AtTabsPane current={current} index={0}>
+            <Skeleton
+              animateName='elastic'
+              title
+              row={2}
+              rowWidth={["100%", "50%"]}
+              loading={isQuestionLoading}
+            >
+              <View className='question-item-container'>
+                <Question content={questions.example.content}></Question>
+              </View>
+            </Skeleton>
+          </AtTabsPane>
+          <AtTabsPane current={current} index={1}>
+            <Skeleton
+              animateName='elastic'
+              title
+              row={2}
+              rowWidth={["100%", "50%"]}
+              loading={isQuestionLoading}
+            >
+              <View className='question-item-container'>
+                <Question content={questions.real.content}></Question>
+              </View>
+            </Skeleton>
+          </AtTabsPane>
+        </AtTabs>
+      </View>
+    );
+  }
+}
--- src/pages/steps/binarization/index.jsx
+++ src/pages/steps/binarization/index.jsx
@@ -1,59 +1,19 @@
 import Taro, { Component } from "@tarojs/taro";
-import { View } from "@tarojs/components";
 import StepPage from "@/components/StepPage";
-import Article from "@/components/Article";
-import { AtAccordion } from "taro-ui";
+import Problem from "@/components/Problem";
+import PROBLEM_CONSTANT from "../../../constants/problem";
 import "./index.scss";
 
 export default class Binarization extends Component {
-  config = {
-    navigationBarTitleText: ""
-  };
-
-  state = {
-    data: {
-      title: "什么是二值化？为什么要将二进制转化为十进制？",
-      sections: [
-        "我们对第二层的每个输出图片都进行二值化处理，得到的结果中只包含一和零（大于等于零的数取1，小于零的数取0），于是就得到了二值化的图，然后将二值化的图片乘以不同的权重（例如：2的七次方等于128，2的六次方64。32，16，8，4，2，1）。将每张图片对应位置的像素值相加，这样得到的图片每个像素都由十进制的值来表示。"
-      ]
-    },
-    exampleOpen: false,
-    solutionOpen: false
-  };
-
-  handleAccordionOpen(key) {
-    this.setState({
-      [key]: !this.state[key]
-    });
-  }
 
   handleNextClick(callback) {
     callback(true);
   }
 
   render() {
-    const { data, exampleOpen, solutionOpen } = this.state;
     return (
       <StepPage onNext={this.handleNextClick.bind(this)}>
-        <View className='question-container'>
-          <View className='question-explain'>
-            <Article title={data.title} sections={data.sections}></Article>
-          </View>
-          <AtAccordion
-            open={exampleOpen}
-            onClick={this.handleAccordionOpen.bind(this, "exampleOpen")}
-            title='题目举例'
-          >
-            <Article title={data.title} sections={data.sections}></Article>
-          </AtAccordion>
-          <AtAccordion
-            open={solutionOpen}
-            onClick={this.handleAccordionOpen.bind(this, "solutionOpen")}
-            title='实战解答'
-          >
-            <Article title={data.title} sections={data.sections}></Article>
-          </AtAccordion>
-        </View>
+        <Problem data={PROBLEM_CONSTANT.BINARIZATION.DATA} questionApi={PROBLEM_CONSTANT.BINARIZATION.QUESTION_API}></Problem>
       </StepPage>
     );
   }
