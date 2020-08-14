9db8c077b772e69353a81eb705eea4ff87b602b0
Hexwell <lazysloths@yahoo.com>
2020-08-14 23:06:14 +0800

    feat: ready for producation

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,12 @@
 /*
  * @Autor: 陈鹏宇
  * @Date: 2020-07-15 14:11:53
- * @LastEditTime: 2020-08-14 20:52:50
+ * @LastEditTime: 2020-08-14 22:48:10
  * @LastEditors: 陈鹏宇
  * @Description: 配置文件
  * @Version: 1.0
  */ 
 
-export const BASE_API = "http://192.168.31.63:8888"
+export const BASE_API = "https://api.fengkuangqie.cn"
 
 export const MD_RENDER_API = "https://md.fengkuangqie.cn/?tex"
