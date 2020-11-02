a8d310a8b0ab86be68a754fd10227378838c39be
Hexwell <pocoa@foxmail.com>
2020-11-02 18:21:38 +0800

    fix(denpendency): fix

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
 import { Provider } from '@tarojs/redux'
-import '@tarojs/async-await'
-import 'taro-ui/dist/style/index.scss'
 import Index from './pages/index'
 import store from './utils/store/store'
 import './app.scss'
@@ -12,12 +10,11 @@ import './app.scss'
 //   require('nerv-devtools')
 // }
 
-const reduxStore = store
 
 class App extends Component {
 
   componentDidMount() {
-    if(Taro.env == 'weapp'){
+    if (Taro.env == 'weapp') {
       this.updateWeapp()
     }
   }
@@ -102,7 +99,7 @@ class App extends Component {
   // 请勿修改此函数
   render() {
     return (
-      <Provider store={reduxStore}>
+      <Provider store={store}>
         <Index />
       </Provider>
     )
